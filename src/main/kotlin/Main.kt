07d256f098de9787aaa079ae048d6d0dc6539d80
OldM1lk <MrlaimWin@gmail.com>
2024-09-08 15:14:48 +0700

    Added new branch and code for task1.3

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,19 @@
 package org.example
 
 fun main() {
+    val randomNumber = (0..10).random()
 
+    while (true) {
+        print("Введите число от 0 до 10: ")
+        val userInput = readln().toInt()
+
+        if (userInput > randomNumber) {
+            println("Много")
+        } else if (userInput < randomNumber) {
+            println("Мало")
+        } else {
+            println("Угадал")
+            break
+        }
+    }
 }
