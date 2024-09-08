c008eaf463b03949d1b7cfee101a7a09cddf6f7c
OldM1lk <MrlaimWin@gmail.com>
2024-09-08 15:02:47 +0700

    Added new branch and code for task1.2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,28 @@
 package org.example
 
 fun main() {
+    var count = 0
+    var sum = 0
 
+    while (true) {
+        print("Введите число: ")
+        val number = readln().toInt()
+
+        if (number == 0) {
+            break
+        }
+
+        sum += number
+        ++count
+    }
+
+    if (count > 0) {
+        val average = sum / count
+
+        println("Количество введенных чисел: $count")
+        println("Сумма введенных чисел: $sum")
+        println("Среднее арифметическое: $average")
+    } else {
+        println("Вы не ввели ни одного числа.")
+    }
 }
