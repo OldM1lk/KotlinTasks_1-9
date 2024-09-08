d24cdac9d8088a3878756fa0e97aa979a0c01a4f
OldM1lk <MrlaimWin@gmail.com>
2024-09-08 14:52:53 +0700

    Added new branch and code for task1.1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,13 @@
 package org.example
 
 fun main() {
-
+    print("Введите число: ")
+    val number = readln().toInt()
+    val lastDigit = number % 10
+    var firstDigit = number
+    while (firstDigit >= 10) {
+        firstDigit /= 10
+    }
+    val sum = firstDigit + lastDigit
+    print("Сумма первой и последней цифры: $sum")
 }
