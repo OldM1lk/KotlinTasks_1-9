b7ad4bf989e4ab37fc84d5261b383a8c9fb07ea2
OldM1lk <MrlaimWin@gmail.com>
2024-09-08 18:05:24 +0700

    Added new branch and code for task2.3

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,56 @@
 package org.example
 
 fun main() {
+    val array = intArrayOf(3, 5, 2, 8, 1)
+    var productFor = 1
+    var minFor = array[0]
+    var maxFor = array[0]
 
+    for (i in 0 until array.size) {
+        productFor *= array[i]
+
+        if (array[i] < minFor) {
+            minFor = array[i]
+        }
+        if (array[i] > maxFor) {
+            maxFor = array[i]
+        }
+    }
+    println("Используя цикл for: произведение: $productFor; мин: $minFor; макс: $maxFor")
+
+    var i = 0
+    var productWhile = 1
+    var minWhile = array[0]
+    var maxWhile = array[0]
+    while (i < array.size)
+    {
+        productWhile *= array[i]
+        if (array[i] < minWhile) {
+            minWhile = array[i]
+        }
+        if (array[i] > maxWhile) {
+            maxWhile = array[i]
+        }
+        ++i
+    }
+    println("Используя цикл while: произведение: $productWhile; мин: $minWhile; макс: $maxWhile")
+
+    val productReduce = array.reduce{a, b -> a * b}
+    val minReduce = array.minOrNull()
+    val maxReduce = array.maxOrNull()
+    println("Используя reduce и min, max: произведение: $productReduce; мин: $minReduce; макс: $maxReduce")
+
+    var productForEach = 1
+    var minForEach = array[0]
+    var maxForEach = array[0]
+    array.forEach {
+        productForEach *= it
+        if (it < minForEach) {
+            minForEach = it
+        }
+        if (it > maxForEach) {
+            maxForEach = it
+        }
+    }
+    println("Используя цикл forEach: произведение: $productForEach; мин: $minForEach; макс: $maxForEach")
 }
