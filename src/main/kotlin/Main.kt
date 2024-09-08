95b192eb6654f4152b1bd00a14c1390c3951eec4
OldM1lk <MrlaimWin@gmail.com>
2024-09-08 17:16:07 +0700

    Added new branch and code for task2.1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,39 @@
 package org.example
 
-fun main() {
+fun foundPrimes(n: Int): ArrayList<Int> {
+    val primeNumbers = arrayListOf(0)
+    val primes = BooleanArray(n) { true }
+    primes[0] = false
+    primes[1] = false
 
+    for (i in 2 until n) {
+        if (primes[i]) {
+            var j = i * i
+
+            if (j < n) {
+                while (j < n) {
+                    primes[j] = false
+                    j += i
+                }
+            }
+        }
+    }
+    primeNumbers.clear()
+
+    for (i in 2 until n) {
+        if (primes[i]) {
+            primeNumbers.add(i)
+        }
+    }
+    return primeNumbers
+}
+
+fun main() {
+    print("Введите количество простых чисел: ")
+    val numbersCount = readln().toInt()
+    val result = foundPrimes(numbersCount)
+
+    for (i in 0 until result.size) {
+        println("${i+1} - ое число: ${result[i]}")
+    }
 }
