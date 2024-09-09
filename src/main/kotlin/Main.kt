5054cbaaf2394f20cd88395f82bb2c01bcb0be73
OldM1lk <MrlaimWin@gmail.com>
2024-09-09 22:08:25 +0700

    Added new branch and code for task9.5

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,14 @@
 package org.example
 
 fun main() {
+    val phoneBook = mapOf(
+        "Alice" to "+1-234-567-8901",
+        "Bob" to "+44-123-456-7890",
+        "Charlie" to "+1-987-654-3210",
+        "David" to "+33-123-456-7890"
+    )
 
+    val countryCode = "+1"
+
+    println(phoneBook.filterValues { it.startsWith(countryCode) })
 }
