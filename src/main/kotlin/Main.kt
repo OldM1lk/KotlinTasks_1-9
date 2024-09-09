bd59a4d97f8ac9aaf6c9744fccaac85a8e3c571c
OldM1lk <MrlaimWin@gmail.com>
2024-09-09 21:39:59 +0700

    Added new branch and code for task8.2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,33 @@
 package org.example
 
 fun main() {
+    val words: List<String?> = listOf("когда", null, "эти", "задания", null, "уже", "закончатся")
 
+    println("Используя оператор if:")
+    for (word in words) {
+        if (word != null) {
+            println(word.uppercase())
+        }
+    }
+
+    println()
+    println("Используя оператор безопасного вызова ?:")
+    for (word in words) {
+        println(word?.uppercase())
+    }
+
+    println()
+    println("Используя функцию let:")
+    for (word in words) {
+        word?.let {
+            println(it.uppercase())
+        }
+    }
+
+    println()
+    println("Используя Элвис-оператор ?:")
+    for (word in words) {
+        val result = word?.uppercase() ?: "EMPTY"
+        println(result)
+    }
 }
