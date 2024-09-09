c9d7412e06fdb80aee4c3b1b8160a26139899f14
OldM1lk <MrlaimWin@gmail.com>
2024-09-09 21:45:06 +0700

    Added new branch and code for task9.1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,9 @@
 package org.example
 
 fun main() {
+    val numbers = listOf(10, -5, 3, -2, 0, -8, 7)
+    println("Исходный список: ${numbers}")
 
+    val negativeNumbers = numbers.filter { it < 0 }
+    println("Отрицательные элементы списка: ${negativeNumbers}")
 }
