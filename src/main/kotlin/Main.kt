99de96225b5847681cbf71b3baecc6cb07bfee2f
OldM1lk <MrlaimWin@gmail.com>
2024-09-09 22:17:42 +0700

    Added new branch and code for task9.5

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,39 @@
 package org.example
 
-fun main() {
+fun numberToWords(number: Int): String {
+    val ones = arrayOf(
+        "ноль", "один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять"
+    )
+    val teens = arrayOf(
+        "десять", "одиннадцать", "двенадцать", "тринадцать", "четырнадцать", "пятнадцать",
+        "шестнадцать", "семнадцать", "восемнадцать", "девятнадцать"
+    )
+    val tens = arrayOf(
+        "", "десять", "двадцать", "тридцать", "сорок", "пятьдесят", "шестьдесят",
+        "семьдесят", "восемьдесят", "девяносто"
+    )
 
+    return when {
+        number == 100 -> "сто"
+        number < 10 -> ones[number]
+        number in 10..19 -> teens[number - 10]
+        else -> {
+            val tenPart = number / 10
+            val onePart = number % 10
+            val tenText = tens[tenPart]
+            val oneText = ones[onePart]
+            if (onePart == 0) {
+                tenText
+            } else {
+                "$tenText $oneText"
+            }
+        }
+    }
+}
+
+fun main() {
+    print("Введите число в диапазоне от 0 до 100: ")
+    val input = readln().toInt()
+
+    println(numberToWords(input))
 }
