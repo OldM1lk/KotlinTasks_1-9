f6373b0628df1de5e0bae05441547e7c7641f12a
OldM1lk <MrlaimWin@gmail.com>
2024-09-09 21:51:24 +0700

    Added new branch and code for task9.2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,8 @@
 package org.example
 
 fun main() {
+    val numbers = listOf(10, -5, 3, -2, 0, -8, 7)
+    println("Исходный список: ${numbers}")
 
+    println("Измененный список: ${numbers.map { if (it > 0) -it else it }}")
 }
