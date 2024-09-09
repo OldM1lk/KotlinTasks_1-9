01a8e6d44e6bdcceffbf6b69e8cf4ef0b6d59785
OldM1lk <MrlaimWin@gmail.com>
2024-09-09 20:44:37 +0700

    Added new branch and code for task6.1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,51 @@
 package org.example
 
-fun main() {
+abstract class Figure {
 
+    abstract fun area(): Double
+    abstract fun perimeter(): Double
+}
+
+class Square(private val side: Double) : Figure() {
+
+    override fun area(): Double {
+        return side * side
+    }
+
+    override fun perimeter(): Double {
+        return 4 * side
+    }
+}
+
+class Circle(private val radius: Double) : Figure() {
+
+    override fun area(): Double {
+        return Math.PI * radius * radius
+    }
+
+    override fun perimeter(): Double {
+        return 2 * Math.PI * radius
+    }
+}
+
+class Triangle(private val a: Double, private val b: Double, private val c: Double) : Figure() {
+
+    override fun area(): Double {
+        val s = perimeter() / 2
+        return Math.sqrt(s * (s - a) * (s - b) * (s - c))
+    }
+
+    override fun perimeter(): Double {
+        return a + b + c
+    }
+}
+
+fun main() {
+    val square = Square(5.0)
+    val circle = Circle(3.0)
+    val triangle = Triangle(3.0, 4.0, 5.0)
+
+    println("Площадь квадрата: ${square.area()}; его периметр: ${square.perimeter()}")
+    println("Площадь круга: ${circle.area()}; его периметр: ${circle.perimeter()}")
+    println("Площадь треугольника: ${triangle.area()}; его периметр: ${triangle.perimeter()}")
 }
