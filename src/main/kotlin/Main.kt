de2d547a2608ff6557c57fbad98478945c2f65ad
OldM1lk <MrlaimWin@gmail.com>
2024-09-09 21:29:12 +0700

    Added new branch and code for task8.1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,4 +2,24 @@ package org.example
 
 fun main() {
 
+    print("Введите первое число: ")
+    val n1 = readln().toDouble()
+
+    print("Введите второе число: ")
+    val n2 = readln().toDouble()
+
+    print("Введите знак операции (+, -, *, /): ")
+    val operation = readln()
+
+    val result = when (operation) {
+        "+" -> n1 + n2
+        "-" -> n1 - n2
+        "*" -> n1 * n2
+        "/" -> n1 / n2
+        else -> {
+             return
+        }
+    }
+
+    println("Результат: $result")
 }
