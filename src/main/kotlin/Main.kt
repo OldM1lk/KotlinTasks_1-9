c31f0087dfe5affaf86782447737bda48b4c9bf9
OldM1lk <MrlaimWin@gmail.com>
2024-09-09 19:00:13 +0700

    Added new branch and code for task4.2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,41 @@
 package org.example
 
-fun main() {
+class NumberArray(private val numbers: IntArray) {
 
+    fun transformArray(): IntArray {
+        val transformedArray = numbers
+
+        for (i in transformedArray.indices) {
+            if (i % 2 == 0) {
+                transformedArray[i] *= 2
+            } else {
+                transformedArray[i] *= 3
+            }
+        }
+
+        return transformedArray
+    }
+
+    fun maxElement(): Int {
+        return numbers.max()
+    }
+
+    fun minElement(): Int {
+        return numbers.min()
+    }
+
+    val sum: Int
+        get() = numbers.sum()
+
+}
+
+fun main() {
+    val array = intArrayOf(3, 2, 4, 5, 1)
+    val numberArray = NumberArray(array)
+
+    println("Исходный массив: ${array.contentToString()}")
+    println("Максимальный элемент: ${numberArray.maxElement()}")
+    println("Минимальный элемент: ${numberArray.minElement()}")
+    println("Сумма элементов: ${numberArray.sum}")
+    println("Измененный массив: ${numberArray.transformArray().contentToString()}")
 }
