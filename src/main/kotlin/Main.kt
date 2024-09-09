41b9da434e1ddef1a12a0e1786e4df80888510b9
OldM1lk <MrlaimWin@gmail.com>
2024-09-09 22:25:03 +0700

    Added new branch and code for task9.7

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,10 @@
 package org.example
 
 fun main() {
+    print("Введите время в формате ЧАСЫ:МИНУТЫ:СЕКУНДЫ: ")
+    val timeString = readln()
 
+    val (hours, minutes, seconds) = timeString.split(":").map { it.toInt() }
+
+    println("Количество секунд, прошедших с начала дня: ${hours * 3600 + minutes * 60 + seconds}")
 }
