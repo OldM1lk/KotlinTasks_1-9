b632b7b785a7bf31817999bbe7986a67098c5ee6
OldM1lk <MrlaimWin@gmail.com>
2024-09-09 21:53:45 +0700

    Added new branch and code for task9.3

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,8 @@
 package org.example
 
 fun main() {
+    val numbers = listOf(10, -5, 3, -2, 0, -8, 7)
+    println("Исходный список: ${numbers}")
 
+    println("Элементы списка в квадрате: ${numbers.map { it * it }}")
 }
