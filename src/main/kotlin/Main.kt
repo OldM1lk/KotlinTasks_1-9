17d4a4ec2170030eeadcff94fd3a33f58dddec6c
OldM1lk <MrlaimWin@gmail.com>
2024-09-09 19:59:22 +0700

    Added new branch and code for task5

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,71 @@
 package org.example
 
-fun main() {
+open class Vehicle {
 
+    open val speed: Int = 0
+    open val name: String = "Транспортное средство"
+
+    open fun start() {
+        println("$name начал движение со скоростью $speed км/ч")
+    }
+
+    open fun stop() {
+        println("$name остановилось")
+    }
+}
+
+class Boat : Vehicle() {
+    override val speed: Int = 20
+    override val name: String = "Лодка"
+
+    override fun start() {
+        println("$name начала плыть со скоростью $speed км/ч")
+    }
+
+    override fun stop() {
+        println("$name остановилась")
+    }
+}
+
+class Airplane : Vehicle() {
+    override val speed: Int = 800
+    override val name: String = "Самолет"
+
+    override fun start() {
+        println("$name взлетел со скоростью $speed км/ч")
+    }
+
+    override fun stop() {
+        println("$name приземлился")
+    }
+}
+
+class Tank : Vehicle() {
+    override val speed: Int = 30
+    override val name: String = "Танк"
+
+    override fun start() {
+        println("$name начал движение со скоростью $speed км/ч")
+    }
+
+    override fun stop() {
+        println("$name остановился")
+    }
+}
+
+fun main() {
+    val boat = Boat()
+    val airplane = Airplane()
+    val tank = Tank()
+
+    boat.start()
+    boat.stop()
+    println()
+
+    airplane.start()
+    airplane.stop()
+    println()
+
+    tank.start()
+    tank.stop()
 }
